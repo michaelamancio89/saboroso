7d0bd553c824d46626b97fd33e89ac9d5d794ab9
Michael Amâncio <michael89amancio@gmail.com>
2021-01-13 19:24:19 -0300

    create header and footer includes in all pages

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -29,7 +29,9 @@ router.get('/contacts', function(req, res, next){
 
   res.render('contacts', {
 
-    title: 'Contato - Restaurante Saboroso!'
+    title: 'Contato - Restaurante Saboroso!',
+    background: 'images/img_bg_3.jpg',
+    h1:'Diga um oi!'
 
   });
 
@@ -39,7 +41,9 @@ router.get('/menus', function(req, res, next){
 
   res.render('menus', {
 
-    title: 'Menus - Restaurante Saboroso!'
+    title: 'Menus - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1:'Saboreie nosso menu!'
   });
   
 });
@@ -48,7 +52,9 @@ router.get('/reservations', function(req, res, next){
 
   res.render('reservations', {
 
-    title: 'Reservas - Restaurante Saboroso!'
+    title: 'Reservas - Restaurante Saboroso!',
+    background: 'images/img_bg_2.jpg',
+    h1:'Reserve uma Mesa!'
 
   });
   
@@ -58,8 +64,10 @@ router.get('/services', function(req, res, next){
 
   res.render('services', {
 
-    title: 'Serviços - Restaurante Saboroso!'
-    
+    title: 'Serviços - Restaurante Saboroso!',
+    background: 'images/img_bg_1.jpg',
+    h1:'É um prazer poder servir!'
+
   });
   
 });
