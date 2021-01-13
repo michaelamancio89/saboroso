5fa1385d37b2c0ffa50bcb20d2c390833fa3c3c8
Michael Amâncio <michael89amancio@gmail.com>
2021-01-13 19:40:46 -0300

    create includes of pages

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,27 +1,21 @@
 var conn = require('./../inc/db');
 var express = require('express');
+var menus = require('./../inc/menus');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`SELECT * FROM tb_menus ORDER BY title`, (err, results) => {
-
-    if (err) {
-
-      console.error(err);
-
-    }
+  menus.getMenus().then(results => {
 
     res.render('index', {
 
       title: 'Restaurante Saboroso!',
       menus: results
+
     });
 
   });
-
-  res.render('index', { title: 'Restaurante Saboroso' });
 
 });
 
@@ -39,13 +33,19 @@ router.get('/contacts', function(req, res, next){
 
 router.get('/menus', function(req, res, next){
 
-  res.render('menus', {
+  menus.getMenus().then(results => {
 
-    title: 'Menus - Restaurante Saboroso!',
-    background: 'images/img_bg_1.jpg',
-    h1:'Saboreie nosso menu!'
+    res.render('menus', {
+
+      title: 'Menus - Restaurante Saboroso!',
+      background: 'images/img_bg_1.jpg',
+      h1:'Saboreie nosso menu!',
+      menus: results
+      
+    });
+
   });
-  
+
 });
 
 router.get('/reservations', function(req, res, next){
