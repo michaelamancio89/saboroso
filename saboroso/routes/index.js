558994197926a4b7953bc80749c9ae3a62299f41
Michael Amâncio <michael89amancio@gmail.com>
2021-01-14 10:32:30 -0300

    save registers from reservations

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,6 +1,8 @@
 var conn = require('./../inc/db');
 var express = require('express');
 var menus = require('./../inc/menus');
+var reservations = require('./../inc/reservations');
+const { render } = require('./../inc/reservations');
 var router = express.Router();
 
 /* GET home page. */
@@ -11,7 +13,8 @@ router.get('/', function(req, res, next) {
     res.render('index', {
 
       title: 'Restaurante Saboroso!',
-      menus: results
+      menus: results,
+      isHome: true
 
     });
 
@@ -50,6 +53,8 @@ router.get('/menus', function(req, res, next){
 
 router.get('/reservations', function(req, res, next){
 
+  reservations.render(req,res);
+
   res.render('reservations', {
 
     title: 'Reservas - Restaurante Saboroso!',
@@ -57,6 +62,35 @@ router.get('/reservations', function(req, res, next){
     h1:'Reserve uma Mesa!'
 
   });
+  
+});
+
+router.post('/reservations', function(req, res, next){
+
+  if (!req.body.name){
+    reservations.render(req, res,"Digite o nome!");
+  } else if (!req.body.email) {
+    reservations.render(req, res,"Digite o email!");
+  } else if (!req.body.people) {
+    reservations.render(req, res,"Selecione o número de pessoas!");
+  } else if (!req.body.date) {
+    reservations.render(req, res,"Selecione a data!");
+  } else if (!req.body.time) {
+    reservations.render(req, res,"Selecione a hora!");
+  } else {
+
+    reservations.save(req.body).then(results => {
+      
+      req.body= {};
+      
+      reservations.render(req, res, null, "Reserva realizada com sucesso!");
+
+    }).catch(err => {
+
+      reservations.render(req, res, err.message);
+
+    });
+  }
   
 });
 
