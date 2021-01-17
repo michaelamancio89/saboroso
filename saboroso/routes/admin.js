99738fb70ed6a973c90b7fd71e6d25889dc1ab41
Michael Amâncio <michael89amancio@gmail.com>
2021-01-17 19:54:59 -0300

    middleware to access

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -2,9 +2,30 @@ var express = require("express");
 var users = require("./../inc/users");
 var router = express.Router();
 
+router.use(function(req,res,next){
+
+    if (['/login'].indexof(req.url) === -1 && !req.session.user) {
+
+        res.redirect("admin/login");
+
+    } else {
+
+        next();
+    }
+    
+});
+
+router.get("/logout", function (req, res, next){
+
+    delete req.session.user;
+
+    res.redirect("/admin/login");
+});
+
 router.get("/", function(req, res, next){
 
     res.render("admin/index");
+
 });
 
 router.post("/login", function(req, res, next){
