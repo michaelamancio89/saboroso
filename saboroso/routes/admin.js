259c9b824b21b2865f998c2ec266aafa1c63dd98
Michael Amâncio <michael89amancio@gmail.com>
2021-01-25 02:03:42 -0300

    excluding data

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -119,6 +119,20 @@ router.post("/menus", function(req, res, next){
     
 });
 
+router.delete("/menus/:id", function(req, res, next){
+
+    menus.delete(req.params.id).then(results => {
+
+        res.send(results);
+
+    }).catch(err => {
+
+        res.send(err);
+        
+    });
+    
+});
+
 
 
 router.get("/reservations", function(req, res, next){
