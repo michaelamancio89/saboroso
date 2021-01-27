57718bfc224d9c585cba2d198e2ea5c45a394239
Michael Amâncio <michael89amancio@gmail.com>
2021-01-27 01:13:10 -0300

    format date with moment.js

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -2,7 +2,11 @@ var express = require("express");
 var users = require("./../inc/users");
 var admin = require("./../inc/admin");
 var menus = require("./../inc/menus");
+var reservations = require("./../inc/reservations");
+var moment = require("moment");
 var router = express.Router();
+
+moment.locale("pt-BR");
 
 
 router.use(function(req,res,next){
@@ -134,15 +138,47 @@ router.delete("/menus/:id", function(req, res, next){
 });
 
 
-
 router.get("/reservations", function(req, res, next){
 
-    res.render("admin/reservations", admin.getParams(req, {
+    reservations.getReservations().then(data => {
 
-        date: {}
+        res.render("admin/reservations", admin.getParams(req, {
 
-    }));
+            date: {},
+            data,
+            moment
+    
+        }));
+    });
 
+});
+
+router.post("/reservations", function(req, res, next){
+
+    menus.save(req.fields, req.files).then(results => {
+
+        res.send(results);
+
+    }).catch(err => {
+
+        res.send(err);
+        
+    });
+    
+});
+
+router.delete("/reservations/:id", function(req, res, next){
+
+    reservations.delete(req.params.id).then(results => {
+
+        res.send(results);
+
+    }).catch(err => {
+
+        res.send(err);
+        
+    });
+    
 });
 
 router.get("/users", function(req, res, next){
