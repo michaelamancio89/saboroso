e12ee77178cc375d42433be566d6771cf647abea
Michael Amâncio <michael89amancio@gmail.com>
2021-01-28 15:57:54 -0300

    create users router

--- saboroso/inc/users.js
+++ saboroso/inc/users.js
@@ -1,3 +1,4 @@
+const { param } = require('../routes/admin');
 var conn = require('./db');
 
 modeule.exports = {
@@ -51,6 +52,98 @@ modeule.exports = {
             });
 
         });
+
+    },
+
+    getUsers(){
+
+        return new Promise((resolve, reject) => {
+
+            conn.query(`SELECT * FROM tb_users ORDER BY title`, (err, results) => {
+
+                if (err) {
+            
+                  reject(err);
+            
+                }
+            
+                resolve(results);
+            
+              });
+
+        });
+    },
+
+    save(fields, files){
+
+      return new Promise((resolve, reject) => {
+
+        let query, params = [
+
+          fields.name, 
+          fields.email
+
+        ];
+
+        if (parseInt(fields.id) > 0) {
+
+          params.push(fields.id);
+
+          query = `
+              
+              UPDATE tb_users SET name = ?, email = ?
+                WHERE id = ? 
+
+          `; 
+
+        } else {
+
+          query =`
+        
+              INSERT INTO tb_users (name, email, password)
+              VALUES (?, ?, ?)
+
+          `;
+          params.push(fields.password);
+        }
+
+        conn.query(query, params, (err, results) => {
+
+          if (err) {
+            
+            reject(err);
+      
+          }else{
+      
+            resolve(results);
+      
+          };  
+
+        });
+
+      });
+
+    },
+
+    delete(id){
+
+      return new Promise((resolve, reject) => {
+
+        conn.query(`
+
+            DELETE FROM tb_users WHERE id = ?
+        `, [
+          id
+        ], (err, results) => {
+
+          if (err) {
+            reject(err);
+          } else {
+             resolve(results);
+          }
+        });
+
+      });
 
     }
 };
